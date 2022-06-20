813eabcd9f9df2f695a3a5a8e1859724bbf74cd8
Samuel Taboas <droidsam@gmail.com>
2022-06-20 20:41:47 +0200

    add difference tests

--- multi-module/calculator/src/main/java/com/example/maven/IntegerCalculatorImpl.java
+++ multi-module/calculator/src/main/java/com/example/maven/IntegerCalculatorImpl.java
@@ -8,14 +8,17 @@ public class IntegerCalculatorImpl implements Calculator<Integer> {
     @Override
     public Integer sum(Integer integer, Integer t1) {
         if (((long)integer + t1) > Integer.MAX_VALUE){
-            throw new InvalidParameterException();
+            throw new ArithmeticException();
         }
         return integer + t1;
     }
 
     @Override
     public Integer difference(Integer integer, Integer t1) {
-        return null;
+        if(((long) integer - t1) < Integer.MIN_VALUE){
+            throw new ArithmeticException();
+        }
+        return integer - t1;
     }
 
     @Override
--- multi-module/calculator/src/test/java/com/example/maven/IntegerCalculatorImplTest.java
+++ multi-module/calculator/src/test/java/com/example/maven/IntegerCalculatorImplTest.java
@@ -31,13 +31,13 @@ public class IntegerCalculatorImplTest {
         Assert.assertEquals((Integer) (-21), new IntegerCalculatorImpl().sum(-1, -20));
     }
 
-    @Test(expected = InvalidParameterException.class)
+    @Test(expected = ArithmeticException.class)
     public void IntegerCalculatorShouldNotAllowSumToOverflow() {
         new IntegerCalculatorImpl().sum(1, Integer.MAX_VALUE);
         Assert.fail("Sum overflow should not be allowed");
     }
 
-    @Test(expected = InvalidParameterException.class)
+    @Test(expected = ArithmeticException.class)
     public void IntegerCalculatorShouldNotAllowSumToOverflowSummingIntegerMaxValue() {
         new IntegerCalculatorImpl().sum(2, Integer.MAX_VALUE - 1);
         Assert.fail("Sum overflow should not be allowed");
@@ -51,6 +51,35 @@ public class IntegerCalculatorImplTest {
     @Test
     public void IntegerCalculatorSumIntegerMaxValueWithNegativeIntegerMaxValueEqualsZero() {
         Assert.assertEquals((Integer) 0, new IntegerCalculatorImpl().sum(-Integer.MAX_VALUE, Integer.MAX_VALUE));
+    }
+
+
+    @Test
+    public void IntegerCalculatorCalculatesDifferenceTwoIntegers() {
+        Assert.assertEquals((Integer) 0, new IntegerCalculatorImpl().difference(1, 1));
+        Assert.assertEquals((Integer) (-19), new IntegerCalculatorImpl().difference(1, 20));
+    }
+
+    @Test
+    public void IntegerCalculatorCalculatesDifferenceTwoIntegersWithNegative() {
+        Assert.assertEquals((Integer) 2, new IntegerCalculatorImpl().difference(1, -1));
+        Assert.assertEquals((Integer) (-21), new IntegerCalculatorImpl().difference(-1, 20));
+    }
+
+    @Test
+    public void IntegerCalculatorCalculatesDifferenceTwoIntegersWithBothNegative() {
+        Assert.assertEquals((Integer) 0, new IntegerCalculatorImpl().difference(-1, -1));
+        Assert.assertEquals((Integer) 19, new IntegerCalculatorImpl().difference(-1, -20));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void IntegerCalculatorShouldNotAllowDifferenceToOverflow() {
+        new IntegerCalculatorImpl().difference( Integer.MIN_VALUE, 1);
+        Assert.fail("Difference overflow should not be allowed");
+    }
+    @Test
+    public void IntegerCalculatorDifferenceIntegerMaxValueWithNegativeIntegerMaxValueEqualsZero() {
+        Assert.assertEquals((Integer) 0, new IntegerCalculatorImpl().difference(Integer.MAX_VALUE, Integer.MAX_VALUE));
     }
 
 
